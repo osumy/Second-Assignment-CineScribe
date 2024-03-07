14776c53f01ed600d7f81412d32db16fd085fe31
AmirAli Araghi <amirali40223@gmail.com>
2024-03-07 23:31:44 +0330

    Modified: Movie.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         while (true) {
             switch (runMenu()){
                 case 3:
@@ -22,46 +23,84 @@ public class Main {
 
         System.out.println("*************************");
         System.out.println("CineScribe");
-        System.out.println(" 1. movie");
-        System.out.println(" 2. actor/actress");
+        System.out.println(" 1. Movie");
+        System.out.println(" 2. Actor/Actress");
         System.out.println(" 3. Exit\n");
 
         System.out.print(">> Enter your choice: ");
         return sc.nextInt();
     }
 
-    public static void movieMenu(){
+    public static void movieMenu() throws IOException {
         Movie movie = new Movie();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("*************************");
         System.out.print(">> Enter the Movie Title: ");
         String movieTitle = sc.nextLine();
-        movie.setAll(movie.getMovieData(movieTitle));
+        String movieData = movie.getMovieData(movieTitle);
+
+        if (movieData == null){
+            System.out.println("Error: Could not connect to api!");
+            return;
+        }
+        else if (!movie.errorCheck(movieData)) {
+            System.out.println("Error: Movie not found!");
+            return;
+        }
+
+        movie.setAll(movieData);
 
         System.out.println("*************************");
-        System.out.println("Movie: " + movie.movieTitle + " (" + movie.year + ")");
-        System.out.println(movie.rated);
+        System.out.println("Movie: " + movie.movieTitle + " (" + movie.year + ") - " + movie.rated);
         System.out.print("Genre: ");
         for (int i = 0; i < movie.genre.size(); i++) {
             if (i > 0)
-                System.out.println(", ");
+                System.out.print(", ");
             System.out.print(movie.genre.get(i));
         }
         System.out.println("\nDirector: " + movie.director);
         System.out.print("Writer: ");
         for (int i = 0; i < movie.writer.size(); i++) {
             if (i > 0)
-                System.out.println(", ");
+                System.out.print(", ");
             System.out.print(movie.writer.get(i));
         }
-        System.out.print("Stars: ");
+        System.out.print("\nStars: ");
         for (int i = 0; i < movie.actorsList.size(); i++) {
             if (i > 0)
-                System.out.println(", ");
-            System.out.print(movie.writer.get(i));
+                System.out.print(", ");
+            System.out.print(movie.actorsList.get(i));
         }
-        System.out.println(" 3. Exit\n");
+        System.out.println("\n" + movie.plot);
+        System.out.println("Release Date: " + movie.releaseDate);
+        System.out.println("Duration: " + movie.runtime);
+        System.out.println("Internet Movie Database Rating: " + movie.rating);
+        System.out.println("IMDB Votes: " + movie.imdbVotes);
+        System.out.print("Language: ");
+        for (int i = 0; i < movie.language.size(); i++) {
+            if (i > 0)
+                System.out.print(", ");
+            System.out.print(movie.language.get(i));
+        }
+        System.out.print("\nCountry: ");
+        for (int i = 0; i < movie.country.size(); i++) {
+            if (i > 0)
+                System.out.print(", ");
+            System.out.print(movie.country.get(i));
+        }
+        System.out.println("\nAwards: " + movie.awards);
+        System.out.println("\nWould you like to know more about the cast? (Enter the desired number. Also, enter b to return to the main menu.)");
+        for (int i = 0; i < movie.actorsList.size(); i++) {
+            System.out.print(" ");
+            System.out.print(i+1);
+            System.out.print(". " + movie.actorsList.get(i) + "\n");
+        }
+        System.out.print(">> ");
+        String str = sc.next();
+        if (str.equals("b")){
+            return;
+        }
     }
 
     public static void actorMenu(){
